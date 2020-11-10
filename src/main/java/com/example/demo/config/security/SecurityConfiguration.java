bb74275b3261cc2d86c18081adaa2105dcc3694e
LucasE2996 <lucas.rosa.2996@gmail.com>
2020-11-09 21:18:26 -0300

    Apply filter to URLs

--- src/main/java/com/example/demo/config/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/security/SecurityConfiguration.java
@@ -1,6 +1,10 @@
 package com.example.demo.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,4 +13,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
+    // Authorization config
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topics").permitAll()
+                .antMatchers(HttpMethod.GET, "/topics/*").permitAll();
+    }
+
+    // Authentication config
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    // Static resources config (js, css, images, etc)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
 }
