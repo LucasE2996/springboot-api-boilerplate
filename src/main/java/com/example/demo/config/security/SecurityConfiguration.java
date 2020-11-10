a268154cc24daf75e7fc1f9ec7d12a845e51d6a2
LucasE2996 <lucas.rosa.2996@gmail.com>
2020-11-09 22:09:02 -0300

    implement user log in thourgh basic authentication in the browser

--- src/main/java/com/example/demo/config/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.demo.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,11 +8,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthService authService;
 
     // Authorization config
     @Override
@@ -26,7 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // Authentication config
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(authService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     // Static resources config (js, css, images, etc)
