9c679f476c4033314e9c39910e16eceb26f78792
LucasE2996 <lucas.rosa.2996@gmail.com>
2020-11-30 10:29:34 -0300

    Set up swagger with springfox
    also deleted a non used controller once made to test if the API was working fine.

---
+++ src/main/java/com/example/demo/config/swagger/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.demo.config.swagger;
+
+import com.example.demo.modelo.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public Docket demoApp() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .ignoredParameterTypes(User.class);
+    }
+}
+
