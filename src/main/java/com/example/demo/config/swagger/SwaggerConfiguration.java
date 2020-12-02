39ab7a2209290531cd2fdc0eefa9a92637b12b77
LucasE2996 <lucas.rosa.2996@gmail.com>
2020-12-01 23:46:20 -0300

    add header to requests to pass auth

--- src/main/java/com/example/demo/config/swagger/SwaggerConfiguration.java
+++ src/main/java/com/example/demo/config/swagger/SwaggerConfiguration.java
@@ -4,10 +4,14 @@ import com.example.demo.modelo.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfiguration implements WebMvcConfigurer {
@@ -19,7 +23,16 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
                 .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(User.class);
+                .ignoredParameterTypes(User.class)
+                .globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                .name("Authorization")
+                                .description("Header para Token JWT")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build()));
     }
 }
 
